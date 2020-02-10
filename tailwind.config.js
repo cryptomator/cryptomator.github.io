79f2a3852a970bd1878cc01daed5cb24ed0720ce
Tobias Hagemann <tobias@hagemann.org>
2020-02-10 14:46:54 +0100

    layout improvements, please fix navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '1rem'
+      padding: '1rem',
     },
     extend: {
       colors: {
@@ -11,28 +11,16 @@ module.exports = {
         'tertiary': '#005E71',
         'dark': '#1E2B33', 
       },
-      width: {
-        'container-md': '768px',
-        'container-lg': '1280px',
-      },
     },
     fontFamily: {
       'headline': 'Quicksand, sans-serif',
       'body': 'Nunito Sans, sans-serif',
     },
     screens: {
-      'md': {'min': '768px'},
-      'lg': {'min': '1280px'},
+      'md': { 'min': '768px' },
+      'lg': { 'min': '1280px' },
     },
   },
   variants: {},
-  plugins: [
-    function({ addBase, config }) {
-      addBase({
-        'h1': { fontSize: config('theme.fontSize.2xl'), fontFamily: config('theme.fontFamily.headline'), fontWeight: config('theme.fontWeight.medium') },
-        'h2': { fontSize: config('theme.fontSize.xl'), fontFamily: config('theme.fontFamily.headline'), fontWeight: config('theme.fontWeight.medium') },
-        'h3': { fontSize: config('theme.fontSize.lg'), fontFamily: config('theme.fontFamily.headline'), fontWeight: config('theme.fontWeight.medium') },
-      })
-    }
-  ],
+  plugins: [],
 }
