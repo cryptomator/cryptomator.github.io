dbc018b594b397473386ef9f67e1c78536ff887e
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-02-11 16:57:05 +0100

    set initial downloads tab to current platform

---
+++ assets/js/download.js
@@ -0,0 +1,21 @@
+function guessDownloadTab() {
+    // for a list of possible strings, see https://stackoverflow.com/a/19883965/4014509
+    let platform = navigator.platform ? navigator.platform.toLowerCase() : '';
+    if (platform.indexOf('win') != -1) {
+        return 'win';
+    } else if (platform.indexOf('mac') != -1) {
+        return 'mac';
+    } else if (platform.indexOf('linux') != -1) {
+        return 'linux';
+    } else if (platform.indexOf('android') != -1) {
+        return 'android';
+    } else if (platform.indexOf('iphone') != -1) {
+        return 'ios';
+    } else if (platform.indexOf('ipad') != -1) {
+        return 'ios';
+    } else if (platform.indexOf('ipod') != -1) {
+        return 'ios';
+    } else {
+        return 'win';
+    }
+}
