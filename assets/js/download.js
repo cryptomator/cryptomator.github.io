497b5446ce387bdff7d6aebc5d3fb119320e5f7e
Tobias Hagemann <tobias@hagemann.org>
2020-04-17 11:48:07 +0200

    changed platform detection method

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,17 +1,11 @@
 function guessDownloadTab() {
-  // for a list of possible strings, see https://stackoverflow.com/a/19883965/4014509
-  // workaround needed for ipad and ipados >= 13, see https://stackoverflow.com/a/57924983/1759462
-  let platform = navigator.platform ? navigator.platform.toLowerCase() : '';
-  if (platform.indexOf('iphone') != -1 ||
-      platform.indexOf('ipod') != -1 ||
-      platform.indexOf('ipad') != -1 ||
-      platform.indexOf('mac') != -1 && navigator.maxTouchPoints > 1) {
+  if (navigator.appVersion.indexOf('iPhone') !== -1 || navigator.appVersion.indexOf('iPad') !== -1 || navigator.appVersion.indexOf('iPod') !== -1) {
     return 'ios';
-  } else if (platform.indexOf('android') != -1) {
+  } else if (navigator.appVersion.indexOf('Android') !== -1) {
     return 'android';
-  } else if (platform.indexOf('linux') != -1) {
+  } else if (navigator.appVersion.indexOf('Linux') !== -1 || navigator.appVersion.indexOf('X11') !== -1) {
     return 'linux';
-  } else if (platform.indexOf('mac') != -1) {
+  } else if (navigator.appVersion.indexOf('Mac') !== -1) {
     return 'mac';
   } else {
     return 'win';
