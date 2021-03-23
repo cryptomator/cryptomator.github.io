8d08f1cee78b11717c37fe396e0cc579408e5234
Tobias Hagemann <tobias@hagemann.org>
2021-03-23 14:45:45 +0100

    added self-service for retrieving lost license keys

---
+++ assets/js/keyrecovery.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// requires desktopkey.js or androidkey.js
+
+class KeyRecovery {
+
+  constructor(form, data) {
+    this._form = form;
+    this._data = data;
+    this._paddle = $.ajax({
+        url: 'https://cdn.paddle.com/paddle/paddle.js',
+        cache: true,
+        dataType: 'script'
+    }).then(() => {
+      if (PADDLE_ENABLE_SANDBOX) {
+        window.Paddle.Environment.set('sandbox');
+      }
+      window.Paddle.Setup({ vendor: PADDLE_VENDOR_ID });
+      return window.Paddle;
+    });
+  }
+
+  getUserHistory() {
+    if (!$(this._form)[0].checkValidity()) {
+      $(this._form).find(':input').addClass('show-invalid');
+      return;
+    }
+
+    this._data.inProgress = true;
+    this._data.errorMessage = '';
+    this._data.success = false;
+    this._paddle.then(paddle => {
+      paddle.User.History(this._data.email, null, response => {
+        if (response.success) {
+          this.onGetUserHistorySucceeded(response.message);
+        } else {
+          this.onGetUserHistoryFailed(response.error.message);
+        }
+      });
+    });
+  }
+
+  onGetUserHistoryFailed(error) {
+    this._data.success = false;
+    this._data.errorMessage = error;
+    this._data.inProgress = false;
+  }
+
+  onGetUserHistorySucceeded() {
+    this._data.success = true;
+    this._data.errorMessage = '';
+    this._data.inProgress = false;
+  }
+
+}
