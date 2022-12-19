0ffdeaf213aba6a8a59680a15e7e260d1c4ef257
Julian Raufelder <Julian@Raufelder.com>
2022-12-19 10:52:23 +0100

    Update Keycloak to 20.0.2

--- assets/js/hubsetup.js
+++ assets/js/hubsetup.js
@@ -421,7 +421,7 @@ EOF`;
         'init-config': {condition: 'service_completed_successfully'},
         'postgres': {condition: 'service_healthy'}
       },
-      image: 'ghcr.io/cryptomator/keycloak:20.0.1',
+      image: 'ghcr.io/cryptomator/keycloak:20.0.2',
       command: startCmd,
       volumes: ['kc-config:/opt/keycloak/data/import'],
       deploy: {
@@ -790,7 +790,7 @@ class KubernetesConfigBuilder extends ConfigBuilder {
             }],
             containers: [{
               name: 'keycloak',
-              image: 'ghcr.io/cryptomator/keycloak:20.0.1',
+              image: 'ghcr.io/cryptomator/keycloak:20.0.2',
               command: startCmd,
               ports: [{containerPort: 8080}],
               resources: {
