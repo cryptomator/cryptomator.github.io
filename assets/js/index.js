6dd64d8144cbb8f00e0bc9cf20ff5daf7d965905
Tobias Hagemann <tobias.hagemann@skymatic.de>
2024-07-18 18:09:24 +0200

    fixed github stargazers count in subpages

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,21 +21,3 @@ $('#testimonial-carousel .prev-button').on('click', () => {
   lastItem.prependTo(wrapper);
   lastItem.animate({ marginLeft: 0 }, 300, 'swing');
 });
-
-$(() => {
-  $.getJSON('https://api.github.com/repos/cryptomator/cryptomator', data => {
-    $('#github-stargazers').text(formatNumber(data.stargazers_count));
-  }).fail(() => {
-    console.error('Error fetching repository data.');
-  });
-  function formatNumber(num) {
-    if (num >= 1000 && num < 1000000) {
-      const formatted = (num / 1000).toFixed(1);
-      return formatted.endsWith('.0') ? formatted.slice(0, -2) + 'k' : formatted + 'k';
-    } else if (num >= 1000000) {
-      const formatted = (num / 1000000).toFixed(1);
-      return formatted.endsWith('.0') ? formatted.slice(0, -2) + 'M' : formatted + 'M';
-    }
-    return num;
-  }
-});
