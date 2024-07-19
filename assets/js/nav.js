0173ec6f6017bae71c565540d7a4cde239d167aa
Tobias Hagemann <tobias.hagemann@skymatic.de>
2024-07-19 15:44:20 +0200

    made formatting of github stargazers count localization-aware

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,17 +1,22 @@
-$(() => {
+function determineGitHubStargazersCount(locale) {
   $.getJSON('https://api.cryptomator.org/desktop/repo.json', data => {
-    $('#github-stargazers').text(formatNumber(data.stargazers_count));
+    $('#github-stargazers').text(formatNumber(data.stargazers_count, locale));
   }).fail(() => {
     console.error('Error fetching repository data.');
   });
-  function formatNumber(num) {
+  function formatNumber(num, locale) {
+    let formatted = num;
     if (num >= 1000 && num < 1000000) {
-      const formatted = (num / 1000).toFixed(1);
-      return formatted.endsWith('.0') ? formatted.slice(0, -2) + 'k' : formatted + 'k';
+      formatted = (num / 1000).toFixed(1);
+      formatted = formatted.endsWith('.0') ? formatted.slice(0, -2) + 'k' : formatted + 'k';
     } else if (num >= 1000000) {
-      const formatted = (num / 1000000).toFixed(1);
-      return formatted.endsWith('.0') ? formatted.slice(0, -2) + 'M' : formatted + 'M';
+      formatted = (num / 1000000).toFixed(1);
+      formatted = formatted.endsWith('.0') ? formatted.slice(0, -2) + 'M' : formatted + 'M';
     }
-    return num;
+    if (locale === 'de') {
+      return formatted.replace('.', ',');
+    } else {
+      return formatted;
+    }
   }
-});
+}
