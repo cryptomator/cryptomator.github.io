82285ba1abb170ea0d28e99b34dd612c205ea2ed
Julian Raufelder <Julian@Raufelder.com>
2024-10-30 14:35:28 +0100

    Enhance 404 error handling in Plausible

--- assets/js/plausible404eventlistener.js
+++ assets/js/plausible404eventlistener.js
@@ -1,1 +1,2 @@
+window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
 document.addEventListener('DOMContentLoaded', function () { plausible('404', { props: { path: document.location.pathname } }); });
