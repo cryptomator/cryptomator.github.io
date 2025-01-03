baf14744d7c9918cc38b6a3d03b6265c68224630
tobihagemann <tobihagemann@users.noreply.github.com>
2025-01-03 14:47:19 +0000

    deploy: 392b55721e344125dd1ccc9dfcd1c104596438f0

--- js/404.min.03511f1b7573c2b1242dfede76207544cc6bab15ec8f7f5cc25c47a6949aabfd.js
+++
@@ -1,1 +0,0 @@
-window.plausible=window.plausible||function(){(window.plausible.q=window.plausible.q||[]).push(arguments)},document.addEventListener("DOMContentLoaded",function(){plausible("404",{props:{path:document.location.pathname}})})
