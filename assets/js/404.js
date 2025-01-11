226a4fdedb73ca940a2aa298022cc16b8a5c3a77
Tobias Hagemann <tobias.hagemann@skymatic.de>
2025-01-11 08:19:59 +0100

    added 404 and outbound-link-click events

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,1 +1,2 @@
 document.addEventListener('DOMContentLoaded', function () { plausible('404', { props: { path: document.location.pathname } }); });
+umami.track('404', { path: document.location.pathname });
