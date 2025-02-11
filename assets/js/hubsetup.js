487400826084f6aabc313d0996967bec153d9f02
Julian Raufelder <Julian@Raufelder.com>
2025-02-11 13:11:59 +0100

    Update Keycloak to 26.1.2

--- assets/js/hubsetup.js
+++ assets/js/hubsetup.js
@@ -431,7 +431,7 @@ EOF`;
         'init-config': {condition: 'service_completed_successfully'},
         'postgres': {condition: 'service_healthy'}
       },
-      image: 'ghcr.io/cryptomator/keycloak:25.0.6',
+      image: 'ghcr.io/cryptomator/keycloak:26.1.2',
       command: startCmd,
       volumes: ['kc-config:/opt/keycloak/data/import'],
       deploy: {
@@ -803,7 +803,7 @@ class KubernetesConfigBuilder extends ConfigBuilder {
             }],
             containers: [{
               name: 'keycloak',
-              image: 'ghcr.io/cryptomator/keycloak:25.0.6',
+              image: 'ghcr.io/cryptomator/keycloak:26.1.2',
               command: startCmd,
               ports: [{containerPort: 8080}],
               resources: {
