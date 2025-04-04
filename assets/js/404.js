ca9187a8202fcd75610d0d9770b7878100f8fcbd
Julian Raufelder <Julian@Raufelder.com>
2025-04-04 12:44:29 +0200

    RIP Plausible

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,2 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () { plausible('404', { props: { path: document.location.pathname } }); });
 umami.track('404', { path: document.location.pathname });
