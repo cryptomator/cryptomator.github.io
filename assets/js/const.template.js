d9219ba9dd1cee856463d89e170a63a6d738b035
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2025-08-29 10:38:41 +0200

    syntax error

--- assets/js/const.template.js
+++ assets/js/const.template.js
@@ -2,10 +2,10 @@
 
 const BASE_API_URL = '{{ .Site.Params.baseApiUrl }}';
 const PADDLE_ENABLE_SANDBOX = {{ .Site.Params.paddleEnableSandbox }};
-const PADDLE_TOKEN = {{ .Site.Params.paddleToken }};
+const PADDLE_TOKEN = '{{ .Site.Params.paddleToken }}';
 const PADDLE_VENDOR_ID = {{ .Site.Params.paddleVendorId }};
 const PADDLE_DESKTOP_PRICE_IDS = {{ .Site.Params.paddleDesktopPriceIds | jsonify }};
-const PADDLE_ANDROID_PRICE_ID = {{ .Site.Params.paddleAndroidPriceId }};
+const PADDLE_ANDROID_PRICE_ID = '{{ .Site.Params.paddleAndroidPriceId }}';
 const PADDLE_HUB_SELF_HOSTED_SUBSCRIPTION_PLAN_ID = {{ .Site.Params.paddleHubSelfHostedSubscriptionPlanId }};
 const PADDLE_HUB_MANAGED_SUBSCRIPTION_PLAN_ID = {{ .Site.Params.paddleHubManagedSubscriptionPlanId }};
 const PADDLE_PRICES_URL = '{{ .Site.Params.paddlePricesUrl }}';
