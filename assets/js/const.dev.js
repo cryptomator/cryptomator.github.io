e3d5441a5a8b9b08e776670b7d96cd42be5d6a18
Julian Raufelder <Julian@Raufelder.com>
2025-09-11 11:06:14 +0200

    Revert "Merge branch 'develop'"
    This reverts commit 4e384394f17b2afb6c15d71c094b64b8d6f5f18d, reversing
    changes made to dce30739397ae004d2e6341875ebb07f8e19ee1b.

---
+++ assets/js/const.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const BASE_API_URL = 'http://localhost';
+const PADDLE_ENABLE_SANDBOX = true;
+const PADDLE_VENDOR_ID = 1385;
+const PADDLE_DESKTOP_PRODUCT_IDS = [54582, 54583, 54584];
+const PADDLE_ANDROID_PRODUCT_ID = 9642;
+const PADDLE_HUB_SELF_HOSTED_SUBSCRIPTION_PLAN_ID = 23141;
+const PADDLE_HUB_MANAGED_SUBSCRIPTION_PLAN_ID = 42235;
+const PADDLE_PRICES_URL = 'https://sandbox-checkout.paddle.com/api/2.0/prices';
+const STORE_API_URL = 'http://localhost:8787/api';
+const STRIPE_PK = 'pk_test_JhF3MoFQGw2Is0DB3BSv345P';
+const STRIPE_PLANS = {'EUR': 'plan_GgVY2JfD49bc02', 'USD': 'plan_GgVZwj545E0uH3'};
