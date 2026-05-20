cddb439ed71bf4ccf01b5f0806f662182a8ba049
Julian Raufelder <Julian@Raufelder.com>
2026-05-20 10:37:51 +0200

    Apply suggestions from review

--- assets/js/webinar-datetime.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-window.formatBerlin = (iso, opts) =>
-  new Intl.DateTimeFormat(document.documentElement.lang || 'en', { timeZone: 'Europe/Berlin', ...opts }).format(new Date(iso));
